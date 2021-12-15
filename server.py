70cdf769ec797e96fdebe702ef1aec09726b6fb0
bluepost59 <kaput.kat@gmail.com>
2021-12-15 22:46:07 +0900

    dbに登録する機能追加

--- server.py
+++ server.py
@@ -1,4 +1,9 @@
-from flask import Flask, render_template
+from sqlite3.dbapi2 import connect
+import time as t
+import json
+import sqlite3
+
+from flask import Flask, render_template, request, Response
 
 app = Flask(__name__, static_folder="public")
 
@@ -6,6 +11,35 @@ app = Flask(__name__, static_folder="public")
 @app.route("/")
 def serve_route():
     return render_template("main.html")
+
+
+@app.route("/register", methods=["POST"])
+def api_register():
+    req_data = json.loads(request.data.decode(request.charset))
+
+    conn = sqlite3.connect("todo.db")
+    cursor = conn.cursor()
+
+    cursor.execute(
+        "create table if not exists todo("
+        "time integer, "
+        "title text,"
+        "detail title"
+        ")"
+    )
+
+    cursor.execute(
+        "insert into todo values (:time, :title, :detail)",
+        {
+            "time": t.time(),
+            "title": req_data["title"],
+            "detail": req_data["detail"]
+        }
+    )
+
+    conn.commit()
+
+    return Response(status=200)
 
 
 if __name__ == "__main__":
